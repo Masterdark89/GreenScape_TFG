3618ee832572bc14231223ba39645bcc4c5a4a02
JaimeCastanedo <mordis0812@gmail.com>
2026-04-20 14:56:46 +0200

    chore: empty proyecto_final branch

--- lib/core/runtime_flags.dart
+++
@@ -1,4 +0,0 @@
-import 'runtime_flags_stub.dart'
-    if (dart.library.io) 'runtime_flags_io.dart' as runtime_flags;
-
-bool get isFlutterTestRuntime => runtime_flags.isFlutterTestRuntime;
--- lib/core/runtime_flags_io.dart
+++
@@ -1,3 +0,0 @@
-import 'dart:io';
-
-bool get isFlutterTestRuntime => Platform.environment.containsKey('FLUTTER_TEST');
